dab5e3061125c599772315085d938dc568977f82
NsStudioz <sneve2016@gmail.com>
2022-06-30 18:06:27 +0300

    Additional scripts prepared. CameraMovement.cs implemented successfully!

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementCamera : MonoBehaviour
+public class WaveSpawner : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
