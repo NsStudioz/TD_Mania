790dc4f8658feb8a85b6953768e6f742f9de8125
NsStudioz <sneve2016@gmail.com>
2022-07-18 10:44:46 +0300

    Enemy.cs mechanics implemented.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,18 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject deathEFX;
+    public float startSpeed = 2f;
+
+    [HideInInspector]
+    public float movingSpeed;
+
+    public float enemyHealth = 100f;
+    public int goldToEarn = 100;
+
     void Start()
     {
-        
+        movingSpeed = startSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(float amount)
     {
-        
+        enemyHealth -= amount;
+        if (enemyHealth <= 0f)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        // PlayerStats.Gold += goldToEarn
+
+        GameObject deathEffects = Instantiate(deathEFX, transform.position, Quaternion.identity);
+        Destroy(deathEffects, 3f);
+
+        Destroy(gameObject);
+    }
+
+    internal void SlowEnemyOnLaserHit(float slowPct) // slow percantage on laser hit.
+    {
+        movingSpeed = startSpeed * (1f - slowPct); // percantages can be misleading since in unity the value is between 0 - 1 (and not 0% - 100%);
     }
 }
