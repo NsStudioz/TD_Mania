2eaf78d6c6d6491905e306f3419e2dd969e6b807
NsStudioz <sneve2016@gmail.com>
2022-07-18 16:52:40 +0300

    WaveSpawner script added. WaveSpawner Mechanics implemented.

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+
+public class WaveSpawner : MonoBehaviour
+{
+    // Objects
+    public TMP_Text waveCountDownText;
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    // Waves & Timers:
+    public float upcomingWaveDelay = 5f;
+    private float countDown = 2f;
+    private float enemySpawnDelay = 0.5f; // delay in seconds for each enemy to spawn during wave.
+    private int waveIndex = 0;
+
+    void Update()
+    {
+        if (countDown == 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countDown = upcomingWaveDelay;
+        }
+
+        countDown -= Time.deltaTime;
+
+        countDown = Mathf.Clamp(countDown, 0f, Mathf.Infinity);
+
+        waveCountDownText.text = string.Format("{0:00.00}", countDown); // convert to actual watch like, real world time format.
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+        // PlayerStats.Round++;
+
+        for(int i = 0; i< waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(enemySpawnDelay);
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
