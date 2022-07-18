9083fe425d8b575018cb6130cab4a967d212fbb4
NsStudioz <sneve2016@gmail.com>
2022-07-18 17:20:18 +0300

    Script populated.

--- Assets/Scripts/GoldUI.cs
+++ Assets/Scripts/GoldUI.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GoldUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TMP_Text goldText;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        goldText.text = "$" + PlayerStats.Gold.ToString();
     }
 }
