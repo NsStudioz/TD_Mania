99c0edd2b04c4cf5b5a9454bf740f5471fbf6cf2
NsStudioz <sneve2016@gmail.com>
2022-07-18 17:18:17 +0300

    New Script for important player UI info.

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    
+    public static int Gold;
+    public int startGold = 500;
+
+    public static int Lives;
+    public int startLives = 20;
+
+    public static int Rounds;
+
+    void Start()
+    {
+        Gold = startGold;
+        Lives = startLives;
+
+        Rounds = 0;
+    }
+
+}
