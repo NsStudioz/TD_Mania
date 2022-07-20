1a6507c0345884bdc345db2285eab3771d8512c1
NsStudioz <sneve2016@gmail.com>
2022-07-20 09:48:29 +0300

    Blueprint mechanic for shop implemented.

--- Assets/Scripts/D_Unit_Blueprint.cs
+++ Assets/Scripts/D_Unit_Blueprint.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class D_Unit_Blueprint : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+[System.Serializable]
+public class D_Unit_Blueprint // dont want this script as MonoBehaviour, because we don't want it as a component in unity. We don't need to attach this to any game object.
+{                              // but we definitely do want to be able to see the fields here inside the inspector. therefore we are going to mark this as system.serializable
+                               // and that way, Unity will save and load the value inside of this class for us, and that means that they are visible in the inspector, therefore we can edit them.
+
+    public GameObject prefab;
+    public int cost;
+
+    public GameObject upgradedPrefab;
+    public int upgradeCost;
+
+    public int GetSellAmount()
     {
-        
+        return cost / 2;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
