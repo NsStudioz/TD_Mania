50ed1757f077dc2f38f965a72cd5d8df5e4aa731
NsStudioz <sneve2016@gmail.com>
2022-07-22 18:49:41 +0300

    testing.

--- Assets/EnemyMovement_Test.cs
+++ Assets/EnemyMovement_Test.cs
@@ -8,22 +8,22 @@ public class EnemyMovement_Test : MonoBehaviour
 {
     private Enemy enemy;
 
-    private Transform target;
-    private int wavepointIndex = 0;
+    public Transform target;
+    public int wavepointIndex = 0;
 
     void Start()
     {
         enemy = GetComponent<Enemy>();
 
-        target = Waypoints.waypoints[0]; // pursuing the first waypoint with the index of zero to be equal to target.
+        transform.position = target.GetChild(wavepointIndex).transform.position; // pursuing the first waypoint with the index of zero to be equal to target.
     }
 
     void Update()
     {
-        Vector3 dir = target.position - transform.position;
+        Vector3 dir = target.GetChild(wavepointIndex).transform.position - transform.position;
         transform.Translate(dir.normalized * enemy.movingSpeed * Time.deltaTime, Space.World);
 
-        if (Vector3.Distance(transform.position, target.position) <= 0.1f)
+        if (Vector3.Distance(transform.position, target.GetChild(wavepointIndex).transform.position) <= 0.1f)
         {
             MoveToNextWaypoint();
         }
@@ -34,15 +34,28 @@ public class EnemyMovement_Test : MonoBehaviour
 
     private void MoveToNextWaypoint()
     {
-        if (wavepointIndex >= Waypoints.waypoints.Length - 1) // if reached the destination.
+        if (wavepointIndex == target.childCount - 1) // if reached the destination.
         {
             EndPath();
             return;
         }
 
         wavepointIndex++;
-        target = Waypoints.waypoints[wavepointIndex];
+        transform.position = target.GetChild(wavepointIndex).transform.position;
     }
+
+    /*    private void MoveToNextWaypoint()
+        {
+            if (Vector3.Distance(transform.position, target.position) < 0.1f) // if reached the destination.
+            {
+                wavepointIndex++;
+                transform.position = target.GetChild(wavepointIndex).transform.position;
+                *//*            EndPath();
+                            return;*//*
+            }
+
+
+        }*/
 
     private void EndPath()
     {
