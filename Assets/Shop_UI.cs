7cf773332699b84caff8a76aa6100c254da52df3
NsStudioz <sneve2016@gmail.com>
2022-07-28 16:11:16 +0300

    new script added. New buttons references added.

--- Assets/Shop_UI.cs
+++ Assets/Shop_UI.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shop_UI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // ITEMS PANEL LIST (UP TO 9):
+    Button buttonItem1; // top left
+    Button buttonItem2; // top Center
+    Button buttonItem3; // top right
+    Button buttonItem4; // middle left
+    Button buttonItem5; // middle Center
+    Button buttonItem6; // middle right
+    Button buttonItem7; // bottom left
+    Button buttonItem8; // bottom Center
+    Button buttonItem9; // bottom right
+
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
