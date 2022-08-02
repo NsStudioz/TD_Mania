258f2ccb9a7d578db6e61b41a964f94036e390f8
NsStudioz <sneve2016@gmail.com>
2022-08-02 19:57:58 +0300

    Expanded turret variety for shop.

--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -8,6 +8,13 @@ public class ShopManager : MonoBehaviour
     public D_Unit_Blueprint d_Unit_Turret_Standard;
     public D_Unit_Blueprint d_Unit_Turret_MissileLauncher;
     public D_Unit_Blueprint d_Unit_Turret_WideBeamer;
+    public D_Unit_Blueprint d_Unit_Turret_LaserBeamer;
+    public D_Unit_Blueprint d_Unit_Turret_StandardAuto;
+    public D_Unit_Blueprint d_Unit_Turret_AntiShield_Standard;
+    public D_Unit_Blueprint d_Unit_Turret_AntiShield_StandardAuto;
+    public D_Unit_Blueprint d_Unit_Turret_AntiShield_ShieldDestroyer;
+    public D_Unit_Blueprint d_Unit_Turret_Buffer;
+
 
     ConstructManager constructManager;
 
@@ -34,5 +41,40 @@ public class ShopManager : MonoBehaviour
         constructManager.SelectTurretToBuild(d_Unit_Turret_WideBeamer);
     }
 
+    public void SelectTurretLaserBeamer()
+    {
+        Debug.Log("Wide Beamer Selected");
+        constructManager.SelectTurretToBuild(d_Unit_Turret_LaserBeamer);
+    }
+
+    public void SelectTurretStandardAuto()
+    {
+        Debug.Log("Wide Beamer Selected");
+        constructManager.SelectTurretToBuild(d_Unit_Turret_StandardAuto);
+    }
+
+    public void SelectTurretAntiShieldStandard()
+    {
+        Debug.Log("Wide Beamer Selected");
+        constructManager.SelectTurretToBuild(d_Unit_Turret_AntiShield_Standard);
+    }
+
+    public void SelectTurretAntiShieldStandardAuto()
+    {
+        Debug.Log("Wide Beamer Selected");
+        constructManager.SelectTurretToBuild(d_Unit_Turret_AntiShield_StandardAuto);
+    }
+
+    public void SelectTurretAntiShieldShieldDestroyer()
+    {
+        Debug.Log("Wide Beamer Selected");
+        constructManager.SelectTurretToBuild(d_Unit_Turret_AntiShield_ShieldDestroyer);
+    }
+
+    public void SelectTurretBuffer()
+    {
+        Debug.Log("Wide Beamer Selected");
+        constructManager.SelectTurretToBuild(d_Unit_Turret_Buffer);
+    }
 
 }
