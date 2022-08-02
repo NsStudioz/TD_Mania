8cb6ebaf16166b226bd4630d43431545e7b1ce7f
NsStudioz <sneve2016@gmail.com>
2022-08-02 10:20:31 +0300

    New script created. added new turret buff mechanic BP (Needs more work).

---
+++ Assets/Scripts/D_Unit_Buffer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D_Unit_Buffer : MonoBehaviour
+{
+
+    [Header("Attributes")]
+    //public List<GameObject> defUnitsInRange = new List<GameObject>();
+    public string defendingUnitTag = "Defenders";
+    public float rangeRadius = 1f;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void CheckRangeRadius()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, rangeRadius);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.tag == "Defensers")
+            {
+                //Buff(collider);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, rangeRadius);
+    }
+
+}
