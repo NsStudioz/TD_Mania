599f19ffbbcd7963ca9b42625e5553775fc741ae
NsStudioz <sneve2016@gmail.com>
2022-08-05 15:28:37 +0300

    New script for Shop_UI hide\show mechanic added.

--- Assets/LayoutVisibility.cs
+++ Assets/LayoutVisibility.cs
@@ -6,18 +6,68 @@ public class LayoutVisibility : MonoBehaviour
 {
 
     public GameObject shopPanelGO;
-    public GameObject turretsCatalogGO;
-    public GameObject trapsCatalogGO;
+    public GameObject turretsCatalogGO_Unfold;
+    public GameObject trapsCatalogGO_Unfold;
+    public GameObject turretsCatalogGO_Fold;
+    public GameObject trapsCatalogGO_Fold;
 
-    // Start is called before the first frame update
+    public Animator shopPanelUI;
+
+    public bool panelIsUnfolded;
+    public bool panelIsUnfolded_Turrets;
+    public bool panelIsUnfolded_Traps;
+
+    private void Awake()
+    {
+        shopPanelUI = GetComponent<Animator>();
+
+/*        panelIsUnfolded_Turrets = false;
+        panelIsUnfolded_Traps = false;
+        panelIsUnfolded = false;*/
+
+    }
+
+
     void Start()
     {
-        
+        shopPanelUI.StopPlayback();
+        trapsCatalogGO_Fold.SetActive(false);
+        turretsCatalogGO_Fold.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickTurretsCatalogBtn()
     {
-        
+
+        shopPanelUI.Play("Anim_UnfoldPanel");
+        trapsCatalogGO_Fold.SetActive(true);
+        turretsCatalogGO_Fold.SetActive(true);
+        turretsCatalogGO_Unfold.SetActive(false);
+        trapsCatalogGO_Unfold.SetActive(false);
+
+        // show turret items
+        // hide trap items
     }
+
+    public void OnClickTrapsCatalogBtn()
+    {
+        shopPanelUI.Play("Anim_UnfoldPanel");
+        trapsCatalogGO_Fold.SetActive(true);
+        turretsCatalogGO_Fold.SetActive(true);
+        turretsCatalogGO_Unfold.SetActive(false);
+        trapsCatalogGO_Unfold.SetActive(false);
+        // show trap items
+        // hide turret items
+
+    }
+
+    public void OnClickRetractPanel()
+    {
+        shopPanelUI.Play("Anim_FoldPanel");
+        trapsCatalogGO_Fold.SetActive(false);
+        turretsCatalogGO_Fold.SetActive(false);
+        turretsCatalogGO_Unfold.SetActive(true);
+        trapsCatalogGO_Unfold.SetActive(true);
+    }
+
+
 }
