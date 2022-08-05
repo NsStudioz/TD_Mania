7ac0d29d2f6ad7a693bfe8f4c6e7f1686b734e8c
NsStudioz <sneve2016@gmail.com>
2022-08-05 11:17:26 +0300

    New Script added for shop panel.

---
+++ Assets/LayoutVisibility.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayoutVisibility : MonoBehaviour
+{
+
+    public GameObject shopPanelGO;
+    public GameObject turretsCatalogGO;
+    public GameObject trapsCatalogGO;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
