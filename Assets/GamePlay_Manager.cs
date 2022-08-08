8c6640d9a3c97969c3760536b677be72fbedb800
NsStudioz <sneve2016@gmail.com>
2022-08-08 12:45:50 +0300

    Linked text objects in unity with the following vars in script.

--- Assets/GamePlay_Manager.cs
+++ Assets/GamePlay_Manager.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GamePlay_Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public TMP_Text GoldCountText;  // Gold shown on Screen
+    public TMP_Text wavesCountText; // wave count, when timer ends start new wave.
+    public TMP_Text TimerCountText; // Time left for game\waves to end.
+
+    [SerializeField] private int goldText = 0;
+    [SerializeField] private int wavesText = 0;
+    [SerializeField] private float timerText = 0f;
+
+
     void Start()
     {
-        
+        LinkTextsToVars();
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        LinkTextsToVars();
     }
+
+
+    private void LinkTextsToVars()
+    {
+        GoldCountText.text = goldText.ToString();
+        wavesCountText.text = wavesText.ToString();
+        TimerCountText.text = timerText.ToString();
+    }
+
 }
