f37a0b53c597672cf5477629e92c1aef98308b99
NsStudioz <sneve2016@gmail.com>
2022-08-09 22:09:08 +0300

    Fixed script's variables.

--- Assets/GamePlay_Manager.cs
+++ Assets/GamePlay_Manager.cs
@@ -7,14 +7,16 @@ public class GamePlay_Manager : MonoBehaviour
 {
 
     public TMP_Text GoldCountText;  // Gold shown on Screen
-    public TMP_Text wavesCountText; // wave count, when timer ends start new wave.
+    public TMP_Text healthCountText; // wave count, when timer ends start new wave.
     public TMP_Text TimerCountText; // Time left for game\waves to end.
 
     [SerializeField] int goldText = 0;
-    [SerializeField] private int wavesText = 0;
-    [SerializeField] private float timerText = 0f;
+    private float countdown = 2f;
+    //[SerializeField] private int healthText = 0;
+    //[SerializeField] private float timerText = 0f;
 
-    
+
+
 
     void Start()
     {
@@ -27,14 +29,19 @@ public class GamePlay_Manager : MonoBehaviour
         //LinkTextsToVars();
         goldText = PlayerStats.Gold;
         GoldCountText.text = goldText.ToString();
+        healthCountText.text = PlayerStats.Lives.ToString();
+        TimerCountText.text = string.Format("{0:00.00}", countdown);
     }
 
 
     private void LinkTextsToVars()
     {
         GoldCountText.text = goldText.ToString();
-        wavesCountText.text = wavesText.ToString();
-        TimerCountText.text = timerText.ToString();
+        
+
+        
+        //TimerCountText.text = timerText.ToString();
+        //healthCountText.text = healthText.ToString();
     }
 
 }
