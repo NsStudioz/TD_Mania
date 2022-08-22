e5155abdae3f179d9f47a3aff6166cf2636badef
NsStudioz <sneve2016@gmail.com>
2022-08-22 18:34:06 +0300

    Fixed Trap Mine bug, cleaned script from unnecessary lines.

--- Assets/Scripts/D_Trap_Mine.cs
+++ Assets/Scripts/D_Trap_Mine.cs
@@ -6,28 +6,19 @@ using UnityEngine;
 public class D_Trap_Mine : MonoBehaviour
 {
     [Header("Attributes")]
-    //public List<GameObject> defUnitsInRange = new List<GameObject>();
-    //public string defendingUnitTag = "Defenders";
     [SerializeField] float explosionRadius = 1f;
     [SerializeField] float explosionDamage = 100;
     [SerializeField] float triggerRadius = 0.5f;
-
-    [SerializeField] bool isTriggered;
-
-
-    void Start()
-    {
-        isTriggered = false;
-    }
+    [SerializeField] bool isTriggered = false;
 
     void Update()
     {
         CheckRangeOnEnemyEncounter();
 
-/*        if (isTriggered == true)
+        if (isTriggered == true)
         {
             Destroy(gameObject);
-        }*/
+        }
     }
 
     private void CheckRangeOnEnemyEncounter()
@@ -39,11 +30,9 @@ public class D_Trap_Mine : MonoBehaviour
             {
                 isTriggered = true;
                 Explode();
-                Destroy(gameObject);
             }
         }
     }
-
 
     private void Explode()
     {
@@ -53,7 +42,6 @@ public class D_Trap_Mine : MonoBehaviour
             if (collider.tag == "Attackers")
             {
                 Damage(collider.transform);
-                //isTriggered = false;
             }
         }
     }
@@ -68,18 +56,12 @@ public class D_Trap_Mine : MonoBehaviour
         }
     }
 
-    public void BuffTrap(float buffAmount)
-    {
-        explosionDamage += buffAmount;
-    }
-
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, triggerRadius);
-
+        //
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, explosionRadius);
-        
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);  
     }
 }
