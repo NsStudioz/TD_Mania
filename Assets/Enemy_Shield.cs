cf5025d80b7c151ad2118c1c1b2ce3f049807432
NsStudioz <sneve2016@gmail.com>
2022-08-30 19:21:56 +0300

    Connected script to Enemy script in parent object.

--- Assets/Enemy_Shield.cs
+++ Assets/Enemy_Shield.cs
@@ -3,10 +3,20 @@ using UnityEngine;
 public class Enemy_Shield : MonoBehaviour
 {
     [SerializeField] float shieldHealth;
+    public float range = 1f;
+
+    Enemy enemy;
 
     private void Start()
     {
         gameObject.SetActive(true);
+
+        enemy = GetComponentInParent<Enemy>();
+    }
+
+    private void Update()
+    {
+
     }
 
     public void TakeShieldDamage(float amount)
@@ -21,16 +31,21 @@ public class Enemy_Shield : MonoBehaviour
     private void DestroyShield()
     {
         gameObject.SetActive(false);
+        enemy.hasShield = false;
         //Destroy(gameObject);
     }
 
-/*    private void OnTriggerEnter(Collider collider)
-    {
-        if (collider.tag == "AS_Bullet")
+
+
+
+    /*    private void OnTriggerEnter(Collider collider)
         {
-            TakeShieldDamage(50);
-            Destroy(collider.gameObject);
-        }  
-    }*/
+            if (collider.tag == "AS_Bullet")
+            {
+                TakeShieldDamage(50);
+                Destroy(collider.gameObject);
+            }  
+        }*/
+
 
 }
