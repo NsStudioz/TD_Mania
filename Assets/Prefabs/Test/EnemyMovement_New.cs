a8d3d963f9bc0aa3947368c751b5a03890a3e7c0
NsStudioz <sneve2016@gmail.com>
2022-09-07 13:06:50 +0300

    New enemy movement script made. Enemy moving and pathing mechanic implemented.

---
+++ Assets/Prefabs/Test/EnemyMovement_New.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement_New : MonoBehaviour
+{
+    Enemy enemy;
+    private Transform currentWaypoint;
+
+    [SerializeField] EnemyPaths pathing;
+
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float distanceThreshold = 0.1f;
+
+    void Start()
+    {
+
+        enemy = GetComponent<Enemy>();
+
+        // set initial position to the first waypoint:
+        currentWaypoint = pathing.GetNextWaypoint(currentWaypoint);
+        transform.position = currentWaypoint.position;
+
+        // set the next waypoint target:
+        currentWaypoint = pathing.GetNextWaypoint(currentWaypoint);
+        transform.LookAt(currentWaypoint);
+
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, enemy.movingSpeed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, currentWaypoint.position) < distanceThreshold)
+        {
+            currentWaypoint = pathing.GetNextWaypoint(currentWaypoint);
+            transform.LookAt(currentWaypoint);
+        }
+    }
+}
