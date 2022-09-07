684facc138e6ad0ac47aa04341556fd6d3f515ae
NsStudioz <sneve2016@gmail.com>
2022-09-07 18:50:23 +0300

    Player Health mechanic recreated.

---
+++ Assets/Player_Health.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Health : MonoBehaviour
+{
+
+    [SerializeField] float range = 0f;
+
+    private void Update()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, range);
+        foreach(Collider collider in colliders)
+        {
+            if (collider.CompareTag("Attackers"))
+            {
+                PlayerStats.Lives--;
+                Destroy(collider.gameObject);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+
+}
