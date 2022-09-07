a8c086c275e02088a802ec9c9369721d4b830123
NsStudioz <sneve2016@gmail.com>
2022-09-07 13:06:14 +0300

    New enemy pathing script made.

---
+++ Assets/Prefabs/Test/EnemyPaths.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPaths : MonoBehaviour
+{
+    
+    public Transform GetNextWaypoint(Transform currentWaypoint)
+    {
+        if (currentWaypoint == null)
+        {
+            return transform.GetChild(0);
+        }
+
+        if (currentWaypoint.GetSiblingIndex() < transform.childCount - 1)
+        {
+            return transform.GetChild(currentWaypoint.GetSiblingIndex() + 1);
+        }
+        else
+        {
+            return transform.GetChild(0);
+        }
+
+        //return null;
+    }
+
+}
