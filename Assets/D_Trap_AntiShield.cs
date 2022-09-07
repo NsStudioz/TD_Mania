fbdadf10afbe5b426f98bdb9df48f4028905e644
NsStudioz <sneve2016@gmail.com>
2022-09-07 11:03:21 +0300

    Created "mine - on trigger, explode on enemy shields" mechanic. Works.

--- Assets/D_Trap_AntiShield.cs
+++ Assets/D_Trap_AntiShield.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class D_Trap_AntiShield : MonoBehaviour
@@ -9,28 +7,61 @@ public class D_Trap_AntiShield : MonoBehaviour
     [SerializeField] float triggerRadius;
     [SerializeField] float explosionRadius;
     [SerializeField] bool isTriggered;
-    [SerializeField] bool hasExploded;
-
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] bool Explode;
+    [SerializeField] float explosionDamage = 100f;
 
     void Update()
     {
-        
+        CheckRangeOnEnemyEncounter();
+
+        if (isTriggered)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void CheckRangeOnEnemyEncounter()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, triggerRadius);
+        foreach(Collider collider in colliders)
+        {
+            if(collider.tag == "EnemyShields")
+            {
+                isTriggered = true;
+                ExlodeOnCollider();
+            }
+        }
+    }
+
+    private void ExlodeOnCollider()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider collider in colliders)
+        {
+            if(collider.tag == "EnemyShields")
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    private void Damage(Transform enemyShield)
+    {
+        Enemy_Shield eS = enemyShield.GetComponent<Enemy_Shield>();
+
+        if (eS != null)
+        {
+            eS.TakeShieldDamage(explosionDamage);
+        }
     }
 
     private void OnDrawGizmos()
     {
-
         Gizmos.color = Color.green;
-        //Gizmos.DrawWireSphere
+        Gizmos.DrawWireSphere(transform.position, triggerRadius);
 
         Gizmos.color = Color.red;
-
-
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 
 }
