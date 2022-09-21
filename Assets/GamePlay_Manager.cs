fb477d428a8ae9d51f04e8846aeeaba6fe788f05
NsStudioz <sneve2016@gmail.com>
2022-09-21 18:44:03 +0300

    Implementing Game Over and Game Won mechanics when game session ends.

--- Assets/GamePlay_Manager.cs
+++ Assets/GamePlay_Manager.cs
@@ -6,38 +6,75 @@ using TMPro;
 public class GamePlay_Manager : MonoBehaviour
 {
 
-    [SerializeField] TMP_Text GoldCountText;  // Gold shown on Screen
-    [SerializeField] TMP_Text healthCountText; // wave count, when timer ends start new wave.
-    [SerializeField] TMP_Text TimerCountText; // Time left for game\waves to end.
+    [SerializeField] TMP_Text GoldCountText;   // Gold count
+    [SerializeField] TMP_Text healthCountText; // Remaining Health
+    [SerializeField] TMP_Text TimerCountText;  // Time left for game & waves to end
 
-    [SerializeField] int goldText = 0;
-    private float countdown = 2f;
-    //[SerializeField] private int healthText = 0;
-    //[SerializeField] private float timerText = 0f;
+    [SerializeField] float masterTimer = 0f;
+    [SerializeField] float masterTimerThreshold = 0;
+    [SerializeField] static bool gameOver;
+    [SerializeField] static bool gameWon;
+
 
     void Start()
     {
-        //LinkTextsToVars(); 
+        gameWon = false;
+        gameOver = false;
+        masterTimer = masterTimerThreshold;
     }
 
     void Update()
     {
-        //LinkTextsToVars();
-        goldText = PlayerStats.Gold;
-        GoldCountText.text = goldText.ToString();
-        healthCountText.text = PlayerStats.Lives.ToString();
-        TimerCountText.text = string.Format("{0:00.00}", countdown);
+        LinkTexts();
+
+        SetGameStates();
+
+        masterTimer -= Time.deltaTime;
     }
 
-
-    private void LinkTextsToVars()
+    private void LinkTexts()
     {
-        GoldCountText.text = goldText.ToString();
-        
-
-        
-        //TimerCountText.text = timerText.ToString();
-        //healthCountText.text = healthText.ToString();
+        GoldCountText.text = "$" + PlayerStats.Gold.ToString();
+        healthCountText.text = PlayerStats.Lives.ToString();
+        TimerCountText.text = string.Format("{0:00.00}", masterTimer);
     }
+
+    private void SetGameStates()
+    {
+        if (masterTimer <= 0)
+        {
+            gameWon = true;
+        }
+        else if (PlayerStats.Lives <= 0) 
+        {
+            gameOver = true;
+        }
+    }
+
+
+    public static bool GetGameOver()
+    {
+        return gameOver;
+    }
+
+    public static bool GetGameWon()
+    {
+        return gameWon;
+    }
+
 
 }
+
+    //[SerializeField] int goldText = 0;
+    //private float countdown = 2f;
+    //[SerializeField] private int healthText = 0;
+    //[SerializeField] private float timerText = 0f;
+
+
+/*    private void LinkTexts()
+    {
+        goldText = PlayerStats.Gold;
+        GoldCountText.text = goldText.ToString();
+    //TimerCountText.text = timerText.ToString();
+    //healthCountText.text = healthText.ToString();
+    }*/
