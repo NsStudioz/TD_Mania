4575195461124da4d68133a349d785c5b6594a17
NsStudioz <sneve2016@gmail.com>
2022-09-28 11:40:07 +0300

    New polished Gold Generator created. added animations in code.

--- Assets/Scripts/Defenders/D_Trap_GoldGenerator.cs
+++ Assets/Scripts/Defenders/D_Trap_GoldGenerator.cs
@@ -10,6 +10,19 @@ public class D_Trap_GoldGenerator : MonoBehaviour
     [SerializeField] float upgradeable_DelayTime = 5f; // can be upgraded.
     [SerializeField] int goldToEarn = 10;              // can be upgraded.
 
+    [Header("Animations")]
+    [SerializeField] Animator animController = null;
+    [SerializeField] string animation_IdleName;
+    [SerializeField] string animation_ActivateName;
+    [SerializeField] string animation_BuildName;
+    [SerializeField] string animation_RemoveName;
+    [SerializeField] bool turretReady = false;
+
+    private void OnEnable()
+    {
+        animController.Play(animation_BuildName);
+    }
+
     private void Start()
     {
         delayTimeThreshold = upgradeable_DelayTime;
@@ -17,6 +30,11 @@ public class D_Trap_GoldGenerator : MonoBehaviour
 
     void Update()
     {
+        if (turretReady)
+        {
+            animController.Play(animation_ActivateName);
+        }
+
         delayTimeThreshold -= Time.deltaTime;
 
         if (delayTimeThreshold <= 0)
@@ -24,6 +42,10 @@ public class D_Trap_GoldGenerator : MonoBehaviour
             PlayerStats.Gold += goldToEarn;
             delayTimeThreshold = upgradeable_DelayTime;
         }
+    }
 
+    public void EnableTurret()
+    {
+        turretReady = true;
     }
 }
