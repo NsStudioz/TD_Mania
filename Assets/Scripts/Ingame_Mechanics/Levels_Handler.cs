28ba0d973b448ace280676421476039fd909abae
NsStudioz <sneve2016@gmail.com>
2022-10-06 17:01:30 +0300

    Attached new Animator component to prefab for scene fader functions. Scene fader functions implemented into script.

--- Assets/Scripts/Ingame_Mechanics/Levels_Handler.cs
+++ Assets/Scripts/Ingame_Mechanics/Levels_Handler.cs
@@ -6,23 +6,44 @@ using UnityEngine.SceneManagement;
 public class Levels_Handler : MonoBehaviour
 {
     // RELATED TO LEVEL LOAD FUNCTIONALITIES:
-    const float pauseGame = 0f;
-    const float ResumeGame = 1f;
+    private const float pauseGame = 0f;
+    private const float ResumeGame = 1f;
+    private const int main_Menu_Index = 0;
+    private int levelToLoad;
 
-    public void Retry() // restart loaded scene
+    [SerializeField] int currentSceneIndex;
+
+    [SerializeField] Animator animator;
+
+
+    private void Update()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); 
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public void GoToNextLevel()
+    {
+        FadeLevel(currentSceneIndex++);
+    }
+
+    public void RestartGameSession() // restart current scene
+    {
+        FadeLevel(currentSceneIndex);
     }
 
     public void ReturnToMainMenu()
     {
-        Debug.Log("Go to Menu");
-        //SceneManager.LoadScene(0);
+        FadeLevel(main_Menu_Index);
     }
 
-    public void ResumeGameSession()
+    public void ResumeGameSession() // resume game
     {
         Time.timeScale = ResumeGame;
+    }
+
+    public void PauseGameSession() // pause game
+    {
+        Time.timeScale = pauseGame;
     }
 
     public void IngameMenuFunction()
@@ -30,8 +51,16 @@ public class Levels_Handler : MonoBehaviour
         //
     }
 
-    public void PauseGameSession()
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // LevelFader:
+    private void FadeLevel(int currentSceneIndex)
     {
-        Time.timeScale = pauseGame;
+        levelToLoad = currentSceneIndex;
+        animator.SetTrigger("FadeScene");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
     }
 }
