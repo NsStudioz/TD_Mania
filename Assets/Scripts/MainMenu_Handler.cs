78effa8256b0750fcf6a6a41e700112ca0ca3da9
NsStudioz <sneve2016@gmail.com>
2022-10-08 18:04:43 +0300

    Optimized code. Created Animator and animations code blocks.

--- Assets/Scripts/MainMenu_Handler.cs
+++ Assets/Scripts/MainMenu_Handler.cs
@@ -7,8 +7,6 @@ public class MainMenu_Handler : MonoBehaviour
 {
 
     [Header("Menu Switches")]
-    //[SerializeField] bool isMainMenuOpened;
-    //[SerializeField] bool shopMenu = false;
     [SerializeField] bool playMenu = false;
     [SerializeField] bool optionsMenu = false;
     [SerializeField] bool creditsMenu = false;
@@ -16,9 +14,25 @@ public class MainMenu_Handler : MonoBehaviour
     [Header("Scenes Index")]
     [SerializeField] int mainMenuIndex = 0;
     [SerializeField] int shopMenuIndex = 1;
-    //[SerializeField] int currentSceneIndex = 0;
-
+    //
     [SerializeField] string urlAddress;
+
+    [Header("Animations")]
+    [SerializeField] Animator menu_UI_AnimController;
+    //
+    [SerializeField] string play_UI_On;
+    [SerializeField] string play_UI_Off;
+    //
+    [SerializeField] string options_UI_On;
+    [SerializeField] string options_UI_Off;
+    //
+    [SerializeField] string credits_UI_On;
+    [SerializeField] string credits_UI_Off;
+
+    [Header("UI Objects")]
+    [SerializeField] GameObject play_UI;
+    [SerializeField] GameObject options_UI;
+    [SerializeField] GameObject credits_UI;
 
     public void OpenMainMenu() // When leaving SHOP SCENE;
     {
@@ -30,19 +44,24 @@ public class MainMenu_Handler : MonoBehaviour
         SceneManager.LoadScene(shopMenuIndex);
     }
 
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
     public void OpenPlayMenu()
     {
         playMenu = true;
+        //menu_UI_AnimController.Play(play_UI_On);
     }
 
     public void OpenOptionsMenu()
     {
         optionsMenu = true;
+        //menu_UI_AnimController.Play(options_UI_On);
     }
 
     public void OpenCreditsMenu()
     {
         creditsMenu = true;
+        //menu_UI_AnimController.Play(credits_UI_On);
     }
 
     public void OpenPrivacyPage()
@@ -52,8 +71,40 @@ public class MainMenu_Handler : MonoBehaviour
 
     public void ClosePopUpMenus()
     {
+        if (playMenu == true)
+        {
+            //menu_UI_AnimController.Play(play_UI_Off);
+            playMenu = false;
+        }
+        else if (optionsMenu == true)
+        {
+            //menu_UI_AnimController.Play(options_UI_Off);
+            optionsMenu = false;
+        }
+        else if (creditsMenu == true)
+        {
+            //menu_UI_AnimController.Play(credits_UI_Off);
+            creditsMenu = false;
+        }   
+    }
+
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public void ExitPlayMenu()
+    {
+        menu_UI_AnimController.Play(play_UI_Off);
         playMenu = false;
+    }
+
+    public void ExitOptionsMenu()
+    {
+        menu_UI_AnimController.Play(options_UI_Off);
         optionsMenu = false;
+    }
+
+    public void ExitCreditsMenu()
+    {
+        menu_UI_AnimController.Play(credits_UI_Off);
         creditsMenu = false;
     }
 }
