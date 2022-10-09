ed0aaf5e23dc74650a246965d75af34314e12127
NsStudioz <sneve2016@gmail.com>
2022-10-09 17:41:17 +0300

    New functions created. added code to activate/ deactivate UI windows.

--- Assets/Scripts/MainMenu_Handler.cs
+++ Assets/Scripts/MainMenu_Handler.cs
@@ -49,18 +49,21 @@ public class MainMenu_Handler : MonoBehaviour
     public void OpenPlayMenu()
     {
         playMenu = true;
+        play_UI.SetActive(true);
         //menu_UI_AnimController.Play(play_UI_On);
     }
 
     public void OpenOptionsMenu()
     {
         optionsMenu = true;
+        options_UI.SetActive(true);
         //menu_UI_AnimController.Play(options_UI_On);
     }
 
     public void OpenCreditsMenu()
     {
         creditsMenu = true;
+        credits_UI.SetActive(true);
         //menu_UI_AnimController.Play(credits_UI_On);
     }
 
@@ -75,36 +78,36 @@ public class MainMenu_Handler : MonoBehaviour
         {
             //menu_UI_AnimController.Play(play_UI_Off);
             playMenu = false;
+            play_UI.SetActive(false);
         }
         else if (optionsMenu == true)
         {
             //menu_UI_AnimController.Play(options_UI_Off);
             optionsMenu = false;
+            options_UI.SetActive(false);
         }
         else if (creditsMenu == true)
         {
             //menu_UI_AnimController.Play(credits_UI_Off);
             creditsMenu = false;
+            credits_UI.SetActive(false);
         }   
     }
 
     //---------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    public void ExitPlayMenu()
+    // For Animations:
+    public void DeactivatePlayMenu()
     {
-        menu_UI_AnimController.Play(play_UI_Off);
-        playMenu = false;
+        play_UI.SetActive(false);
     }
 
-    public void ExitOptionsMenu()
+    public void DeactivateOptionsMenu()
     {
-        menu_UI_AnimController.Play(options_UI_Off);
-        optionsMenu = false;
+        options_UI.SetActive(false);
     }
 
-    public void ExitCreditsMenu()
+    public void DeactivateCreditsMenu()
     {
-        menu_UI_AnimController.Play(credits_UI_Off);
-        creditsMenu = false;
+        credits_UI.SetActive(false);
     }
 }
