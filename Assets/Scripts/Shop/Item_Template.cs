ffe605d975358b2eefce42c16c7001bf6f9af331
NsStudioz <sneve2016@gmail.com>
2022-10-20 00:22:40 +0300

    Set new objects and values for prefab for shop menu upgrade button and cost management.

--- Assets/Scripts/Shop/Item_Template.cs
+++ Assets/Scripts/Shop/Item_Template.cs
@@ -10,6 +10,10 @@ public class Item_Template : MonoBehaviour
     public TMP_Text titleText;
     public TMP_Text levelText;
     public TMP_Text costText;
+    public GameObject costTextGO;
+    public TMP_Text UpgradeText;
     public Button upgradeButton;
     public Button statsButton;
+    public int unitLevel = 0;
+
 }
