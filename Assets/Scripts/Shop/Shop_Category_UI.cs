cbf8293cbc809ca7e633d2dfa6ac997e7901edf5
NsStudioz <sneve2016@gmail.com>
2022-10-24 09:21:27 +0300

    Back button for Shop scene implemented.

--- Assets/Scripts/Shop/Shop_Category_UI.cs
+++ Assets/Scripts/Shop/Shop_Category_UI.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Shop_Category_UI : MonoBehaviour
 {
     [SerializeField] GameObject items_TurretCategory;
     [SerializeField] GameObject items_Traps_Category;
     [SerializeField] GameObject items_Arsenal_Category;
+
+    private int mainMenuSceneIndex = 0;
 
     private void Start()
     {
@@ -34,5 +37,10 @@ public class Shop_Category_UI : MonoBehaviour
         items_TurretCategory.SetActive(false);
         items_Traps_Category.SetActive(false);
         items_Arsenal_Category.SetActive(true);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene(mainMenuSceneIndex);
     }
 }
