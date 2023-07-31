69d9b575eafb12bc5684b20c33e401aba233e873
NsStudioz <sneve2016@gmail.com>
2023-07-31 12:44:23 +0300

    Update: EnemyPaths.cs = refactor code.

--- Assets/Scripts/Attackers/EnemyPaths.cs
+++ Assets/Scripts/Attackers/EnemyPaths.cs
@@ -1,27 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyPaths : MonoBehaviour
 {
-    
     public Transform GetNextWaypoint(Transform currentWaypoint)
     {
         if (currentWaypoint == null)
-        {
-            return transform.GetChild(0);
-        }
+            return GetChildTransformIndex(0);
 
         if (currentWaypoint.GetSiblingIndex() < transform.childCount - 1)
-        {
-            return transform.GetChild(currentWaypoint.GetSiblingIndex() + 1);
-        }
+            return GetChildTransformIndex(currentWaypoint.GetSiblingIndex() + 1);
         else
-        {
-            return transform.GetChild(0);
-        }
+            return GetChildTransformIndex(0);
+    }
 
-        //return null;
+    private Transform GetChildTransformIndex(int index)
+    {
+        return transform.GetChild(index);
     }
 
 }
