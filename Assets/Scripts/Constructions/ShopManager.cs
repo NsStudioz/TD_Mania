33236e8b3bd34ff3f75f4ab2d8fd11b998edc222
NsStudioz <sneve2016@gmail.com>
2023-08-01 18:13:43 +0300

    Update: ShopManager.cs = small tweak

--- Assets/Scripts/Constructions/ShopManager.cs
+++ Assets/Scripts/Constructions/ShopManager.cs
@@ -18,66 +18,60 @@ public class ShopManager : MonoBehaviour
     [SerializeField] D_Unit_Blueprint d_Unit_Trap_GoldGenerator;
     [SerializeField] D_Unit_Blueprint d_Unit_Trap_AntiShieldMine;
 
-    ConstructManager constructManager;
-
-    void Start()
-    {
-        constructManager = ConstructManager.instance;
-    }
 
     public void SelectTurretStandard()
     {
-        constructManager.SelectTurretToBuild(d_Unit_Turret_Standard);
+        ConstructManager.instance.SelectTurretToBuild(d_Unit_Turret_Standard);
     }
 
     public void SelectTurretMissileLauncher()
     {
-        constructManager.SelectTurretToBuild(d_Unit_Turret_MissileLauncher);
+        ConstructManager.instance.SelectTurretToBuild(d_Unit_Turret_MissileLauncher);
     }
 
     public void SelectTurretWideBeamer()
     {
-        constructManager.SelectTurretToBuild(d_Unit_Turret_WideBeamer);
+        ConstructManager.instance.SelectTurretToBuild(d_Unit_Turret_WideBeamer);
     }
     public void SelectTurretLaserBeamer()
     {
-        constructManager.SelectTurretToBuild(d_Unit_Turret_LaserBeamer);
+        ConstructManager.instance.SelectTurretToBuild(d_Unit_Turret_LaserBeamer);
     }
     public void SelectTurretStandardAuto()
     {
-        constructManager.SelectTurretToBuild(d_Unit_Turret_StandardAuto);
+        ConstructManager.instance.SelectTurretToBuild(d_Unit_Turret_StandardAuto);
     }
     public void SelectTurretAntiShieldStandard()
     {
-        constructManager.SelectTurretToBuild(d_Unit_Turret_AntiShield_Standard);
+        ConstructManager.instance.SelectTurretToBuild(d_Unit_Turret_AntiShield_Standard);
     }
     public void SelectTurretAntiShieldStandardAuto()
     {
-        constructManager.SelectTurretToBuild(d_Unit_Turret_AntiShield_StandardAuto);
+        ConstructManager.instance.SelectTurretToBuild(d_Unit_Turret_AntiShield_StandardAuto);
     }
     public void SelectTurretAntiShieldShieldDestroyer()
     {
-        constructManager.SelectTurretToBuild(d_Unit_Turret_AntiShield_ShieldDestroyer);
+        ConstructManager.instance.SelectTurretToBuild(d_Unit_Turret_AntiShield_ShieldDestroyer);
     }
     public void SelectTurretBuffer()
     {
-        constructManager.SelectTurretToBuild(d_Unit_Turret_Buffer);
+        ConstructManager.instance.SelectTurretToBuild(d_Unit_Turret_Buffer);
     }
     public void SelectTrapMine()
     {
-        constructManager.SelectTurretToBuild(d_Unit_Trap_Mine);
+        ConstructManager.instance.SelectTurretToBuild(d_Unit_Trap_Mine);
     }
     public void SelectTrapBinder()
     {
-        constructManager.SelectTurretToBuild(d_Unit_Trap_Binder);
+        ConstructManager.instance.SelectTurretToBuild(d_Unit_Trap_Binder);
     }
     public void SelectTrapGoldGenerator()
     {
-        constructManager.SelectTurretToBuild(d_Unit_Trap_GoldGenerator);
+        ConstructManager.instance.SelectTurretToBuild(d_Unit_Trap_GoldGenerator);
     }
     public void SelectTrapAntiShieldMine()
     {
-        constructManager.SelectTurretToBuild(d_Unit_Trap_AntiShieldMine);
+        ConstructManager.instance.SelectTurretToBuild(d_Unit_Trap_AntiShieldMine);
     }
 
 }
